3554d59a02a173bb106976819f8932edee484c27
Lugia0529 <Lugia1216@gmail.com>
2011-04-16 16:55:38 +0800

    [5] Folder name typo.

--- ChatClient/src/Core/Contact.java
+++ ChatClient/src/Core/Contact.java
@@ -20,9 +20,29 @@ public class Contact
         return this.guid;
     }
     
+    public String getUsername()
+    {
+        return this.username;
+    }
+    
+    public String getTitle()
+    {
+        return this.title;
+    }
+    
+    public String getPSM()
+    {
+        return this.psm;
+    }
+    
     public void setTitle(String title)
     {
         this.title = title;
+    }
+    
+    public void setPSM(String psm)
+    {
+        this.psm = psm;
     }
     
     // Overide toString() method, so the Contact list can show proper contact detail instead of instance memory location.
